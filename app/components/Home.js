d7e3666e56ff054e24504b913e5d58f911050f82
andela-jwarugu <joy.warugu@andela.com>
2016-07-26 12:38:40 +0300

    Add routes to project and use props.children

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function () {
+    return (
+      <div>
+        Hello from the other side
+      </div>
+    )
+  }
+});
+
+module.exports = Home;
