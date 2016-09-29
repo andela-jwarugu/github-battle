0849e48df697e2bee8f2baced8f2ccb8df5b305c
andela-jwarugu <joy.warugu@andela.com>
2016-09-29 12:49:35 +0300

    Lifecycle Components

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,6 +7,7 @@ var IndexRoute = ReactRouter.IndexRoute;
 var Main = require ('../components/Main');
 var Home = require ('../components/Home');
 var PromptContainer = require('../containers/PromptContainer');
+var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
 
 var routes = (
   <Router history={hashHistory}>
@@ -14,6 +15,7 @@ var routes = (
       <IndexRoute components={Home} />
       <Route path='player1' header='Player One' components={PromptContainer} />
       <Route path='player2/:player1' header='Player Two' components={PromptContainer} />
+      <Route path='battle' component={ConfirmBattleContainer} />
     </Route>
   </Router>
 )
